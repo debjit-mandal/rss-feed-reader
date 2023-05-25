602c36a58be4194c0d91910e9bd96a1a1842db19
Debjit Mandal <115482933+debjit-mandal@users.noreply.github.com>
2023-05-25 22:09:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import feedparser
 print ('Enter the URL: ')
 url = input()
 feed = feedparser.parse(url)
+try:
+    title = feed['feed']['title']
+    print(title)
+
+except:
+    print('error')
 for item in feed['entries']:
     print (item['title']+ "\n" + item['link']+"\n" + item['description']+"\n")
